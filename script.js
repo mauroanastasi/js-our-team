2d2da1e83c8d07ed6adef3c517f912fb9f1c2db1
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-31 23:38:21 +0200

    [UPD] creo funzione e inseriesco card

--- script.js
+++ script.js
@@ -37,4 +37,29 @@ const teamMembers = [
   }
 ];
 
-console.log(teamMembers)
+// creo la mia funzione per destrutturare
+const generateMemberCard = (member) => {
+// creo una stringa dove inserire la mia card
+const card = `<div class="col-12 col-md-6 col-lg-4">
+                        <div class="team-card text-white" >
+                            <row class="info d-flex bg-dark">
+                                <div class="col-3">
+                                    <img src="./img/female1.png" class="img-fluid" alt="team1">
+                                </div>
+                                <div class="col-9 text-right p-1">
+                                    <p class="mb-0">
+                                        <b>LAURA ROSSI</b>
+                                    </p>                                                                         
+                                    <p class="mb-0">
+                                        Designer
+                                    </p>                                    
+                                    <a href="laurarossi@team.com">laurarossi@team.com</a>
+                                </div>
+                            </row>
+                        </div>
+                    </div>`
+}
+// ciclo gli array richiamando la funzione generate membercard
+for(let i=onabort; i<teamMembers.length;i++){
+  generateMemberCard(teamMembers[i])
+}
